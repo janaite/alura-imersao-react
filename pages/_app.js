eecab00eb22bb50b4b19b51054cd0d3bac2148fa
janaite <janaite@gmail.com>
2021-07-12 21:59:13 -0300

    simple layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9e6f6;
+  }
+
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
   }
 `
 
